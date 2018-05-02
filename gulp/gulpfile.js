5de5de6d86c7243273fc962737befeff612e472e
ryo nakamaru <pottava@gmail.com>
2018-05-02 11:35:56 +0900

    runtime & tools for the local dev environment

---
+++ gulp/gulpfile.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task("api", function() {
+  gulp.src("/monitor/generated/cmd/sigmngr-server/main.go")
+      .pipe(shell(['docker restart api'], {ignoreErrors: true}));
+});
+
+gulp.task("default", function() {
+    gulp.watch("/monitor/controllers/*.go", ["api"]);
+    gulp.watch("/monitor/lib/*.go",         ["api"]);
+});

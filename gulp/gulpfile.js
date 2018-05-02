3cca91580af32f7724bec0d082e490da6fc23a42
ryo nakamaru <pottava@gmail.com>
2018-05-02 17:31:40 +0900

    change monitoring directories

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -4,11 +4,12 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task("api", function() {
-  gulp.src("/monitor/generated/cmd/sigmngr-server/main.go")
+  gulp.src("/monitor/generated/cmd/ngc-registry-server/main.go")
       .pipe(shell(['docker restart api'], {ignoreErrors: true}));
 });
 
 gulp.task("default", function() {
-    gulp.watch("/monitor/controllers/*.go", ["api"]);
-    gulp.watch("/monitor/lib/*.go",         ["api"]);
+    gulp.watch("/monitor/controllers/**/*.go", ["api"]);
+    gulp.watch("/monitor/lib/**/*.go",         ["api"]);
+    gulp.watch("/monitor/ngc/**/*.go",         ["api"]);
 });
